6f5617c5b9ff3c480de407047e1f7034b8e95217
Teemu Laine <teemu.laine@etteplanmore.com>
2020-12-10 00:32:33 +0200

    01 solved. 02 solved.

--- 2020/Common/FileSystem.cs
+++ 2020/Common/FileSystem.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 
 namespace Common
 {
-    public class Class1
+    public class FileSystem
     {
+        public static string GetSolutionPath()
+        {
+            // Hack
+            var exePath = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
+            var solution = Path.Combine(exePath, @"..\..\..\..");
+            return Path.GetFullPath(solution);
+        }
     }
 }
