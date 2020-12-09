409952ac95a6a610e7a2c747d1be52512888461b
Teemu Laine <teemu.laine@etteplanmore.com>
2020-12-09 23:12:07 +0200

    init

---
+++ 2020/01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var input = File.ReadAllLines()
+        }
+    }
+}
