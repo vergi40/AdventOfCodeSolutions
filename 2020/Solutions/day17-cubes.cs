b5700f1b1dbd423136ece40465af6c9bd3a9d1d7
Teemu Laine <teemu.laine@etteplanmore.com>
2020-12-20 16:11:34 +0200

    Solve day 17b.

--- 2020/Solutions/day17-cubes.cs
+++ 2020/Solutions/day17-cubes.cs
@@ -173,11 +173,173 @@ namespace Solutions
                 return $"{X} {Y} {Z}";
             }
         }
+
+        record Pos4(int X, int Y, int Z, int W);
         
         
         public override int SolveB()
         {
-            return 0;
+            var startMaxY = Content.Count;
+            var startMaxX = Content.First().Length;
+
+            // Let's define our grid to start from [0,0,0]
+            var grid = new Grid4(0, startMaxX - 1, 0, startMaxY - 1, 0, 0, 0, 0);
+            grid.Read(Content);
+
+            // Cycles
+            // Stays active if 2 or 3 active
+            // Inactive -> active, if 3 neighbors active
+            for (int i = 0; i < 6; i++)
+            {
+                grid.IncreaseSize();
+                grid.UpdateStates();
+
+                Console.WriteLine($"Cycle {i + 1}. Active cubes: {grid.Cubes.Values.Count(c => c)}");
+            }
+
+
+            return grid.Cubes.Values.Count(c => c);
+        }
+
+        class Grid4
+        {
+            public int MinX { get; set; }
+            public int MaxX { get; set; }
+            public int MinY { get; set; }
+            public int MaxY { get; set; }
+            public int MinZ { get; set; }
+            public int MaxZ { get; set; }
+            public int MinW { get; set; }
+            public int MaxW { get; set; }
+
+            public Dictionary<Pos4, bool> Cubes { get; set; } = new();
+
+            public Grid4(int minX, int maxX, int minY, int maxY, int minZ, int maxZ, int minW, int maxW)
+            {
+                MinX = minX;
+                MaxX = maxX;
+                MinY = minY;
+                MaxY = maxY;
+                MinZ = minZ;
+                MaxZ = maxZ;
+                MinW = minW;
+                maxW = MaxW;
+
+                Cubes = InitializeNew();
+            }
+
+            private Dictionary<Pos4, bool> InitializeNew()
+            {
+                var cubes = new Dictionary<Pos4, bool>();
+                for (int i = MinX; i <= MaxX; i++)
+                {
+                    for (int j = MinY; j <= MaxY; j++)
+                    {
+                        for (int k = MinZ; k <= MaxZ; k++)
+                        {
+                            for (int l = MinW; l <= MaxW; l++)
+                            {
+                                cubes.Add(new Pos4(i, j, k, l), false);
+                            }
+                        }
+                    }
+                }
+
+                return cubes;
+            }
+
+            public void Read(List<string> content)
+            {
+                for (int i = 0; i < content.Count; i++)
+                {
+                    var line = content[i];
+                    for (int j = 0; j < line.Length; j++)
+                    {
+                        if (line[j] == '#')
+                        {
+                            Cubes[new Pos4(i, j, 0, 0)] = true;
+                        }
+                    }
+                }
+            }
+
+            public void IncreaseSize()
+            {
+                MinX -= 1;
+                MaxX += 1;
+                MinY -= 1;
+                MaxY += 1;
+                MinZ -= 1;
+                MaxZ += 1;
+                MinW -= 1;
+                MaxW += 1;
+                var newCubes = InitializeNew();
+
+                foreach (var cube in Cubes)
+                {
+                    if (Cubes[cube.Key])
+                    {
+                        // Active
+                        newCubes[cube.Key] = true;
+                    }
+                }
+
+                Cubes = newCubes;
+            }
+
+            public void UpdateStates()
+            {
+                var newCubes = InitializeNew();
+                foreach (var cube in Cubes)
+                {
+                    var count = CalculateSurroundingActive(cube.Key);
+                    if (cube.Value && (count == 2 || count == 3))
+                    {
+                        // Active
+                        newCubes[cube.Key] = true;
+                    }
+                    else if (!cube.Value && count == 3)
+                    {
+                        newCubes[cube.Key] = true;
+                    }
+                }
+
+                Cubes = newCubes;
+            }
+
+            private int CalculateSurroundingActive(Pos4 pos)
+            {
+                var count = 0;
+
+                for (int i = pos.X - 1; i <= pos.X + 1; i++)
+                {
+                    for (int j = pos.Y - 1; j <= pos.Y + 1; j++)
+                    {
+                        for (int k = pos.Z - 1; k <= pos.Z + 1; k++)
+                        {
+                            for (int l = pos.W - 1; l <= pos.W + 1; l++)
+                            {
+                                // Skip self position
+                                if (pos.X == i && pos.Y == j && pos.Z == k && pos.W == l) continue;
+
+                                if (IsActive(new Pos4(i, j, k, l))) count++;
+                            }
+                        }
+                    }
+                }
+
+                return count;
+            }
+
+            private bool IsActive(Pos4 pos)
+            {
+                // Whitelist only if inside borders
+                if (pos.X < MinX || pos.X > MaxX) return false;
+                if (pos.Y < MinY || pos.Y > MaxY) return false;
+                if (pos.Z < MinZ || pos.Z > MaxZ) return false;
+                if (pos.W < MinW || pos.W > MaxW) return false;
+                return Cubes[pos];
+            }
         }
     }
 }
