8686f6346c926803f4f8f59611c8fb91e4c8edbc
Teemu Laine <teemu.laine@etteplan.com>
2021-12-07 08:57:30 +0200

    Solve day 7

---
+++ 2021_AoC/2021_AoC/Day07.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021_AoC
+{
+    internal class Day07 : DayBase
+    {
+        public Day07() : base("07")
+        {
+        }
+
+        public override long SolveA()
+        {
+            //var positions = "16,1,2,0,4,2,7,1,2,14".Split(",").Select(x => int.Parse(x)).ToList();
+            var positions = Content.First().Split(",").Select(x => int.Parse(x));
+
+            var length = positions.Max();
+
+            var smallest = long.MaxValue;
+            for(int i = 0; i < length; i++)
+            {
+                long fuel = 0;
+                foreach(var position in positions)
+                {
+                    fuel += Math.Abs(position - i);
+                }
+
+                if (fuel < smallest) smallest = fuel;
+            }
+
+            return smallest;
+        }
+
+        public override long SolveB()
+        {
+            var positions = Content.First().Split(",").Select(x => int.Parse(x));
+
+            var length = positions.Max();
+
+            // Precalculate to save time
+            var fuelDict = new FuelDict(length);
+
+            var smallest = long.MaxValue;
+            for (int i = 0; i < length; i++)
+            {
+                long fuel = 0;
+                foreach (var position in positions)
+                {
+                    fuel += fuelDict.Values[Math.Abs(position - i)];
+                }
+
+                if (fuel < smallest) smallest = fuel;
+            }
+
+            return smallest;
+        }
+
+        class FuelDict
+        {
+            public Dictionary<int,int> Values { get; }
+
+            public FuelDict(int length)
+            {
+                Values = new Dictionary<int,int>();
+
+                var previous = 0;
+                for(int i = 0; i <= length; i++)
+                {
+                    previous += i;
+                    Values.Add(i, previous);
+                }
+            }
+        }
+    }
+}
---
+++ 2021_AoC/2021_AoC/Day08.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021_AoC
+{
+    internal class Day08 : DayBase
+    {
+        public Day08() : base("08")
+        {
+        }
+
+        public override long SolveA()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long SolveB()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
