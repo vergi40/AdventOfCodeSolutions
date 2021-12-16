cf8c5b5f933f35096dcc08efac6e7e03ed0cf6c0
Teemu Laine <teemu.laine@etteplan.com>
2021-12-16 23:03:31 +0200

    Solve day 16 F#

--- 2021_AoC/Utils/Interactive.fsx
+++ 2021_AoC/Utils/Interactive.fsx
@@ -1,22 +1,18 @@
 ﻿module Interactive
+open System
 
-let list = [1; 1000; 2; 3; 555; 1010; 1020; 666]
-let list2 = [1; 1000; 2; 3; 555; 1010; 1020; 666]
+let ex1 = "D2FE28"
 
-let solveA list =
-    let list2 = List.pairwise list
-    let increased = List.filter (fun (x,y) -> y > x) list2
-    increased.Length
+// Other methods: Text.Encoding.ASCII.GetBytes, Byte.Parse(hex, Globalization.NumberStyles.AllowHexSpecifier)
+let hexToBinary (c:char) = 
+    let hex = new String(c, 1)
+    let b = Int16.Parse(hex, Globalization.NumberStyles.AllowHexSpecifier)
+    let c = Convert.ToString(b, 2)
+    c
 
-solveA list
 
-let solveB seq =
-    let list = Seq.toList seq
-    let listsOf3 = List.windowed 3 list
-    // Important to define target mapping type (each member is int list)
-    let sumsOf3 = List.map (fun (values: int list) -> (values[0] + values[1] + values[2])) listsOf3
+let solveA (input:string) =
+    hexToBinary input[0]
 
-    let result2 = solveA sumsOf3
-    printfn "%A" result2
 
-solveB list2
+solveA ex1    
