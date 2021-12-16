3b50d68876280e5dbc18371073f45efe5f7b8836
Teemu Laine <teemu.laine@etteplan.com>
2021-12-16 23:22:41 +0200

    fixup binary and padding

--- 2021_AoC/Utils/Interactive.fsx
+++ 2021_AoC/Utils/Interactive.fsx
@@ -8,11 +8,26 @@ let hexToBinary (c:char) =
     let hex = new String(c, 1)
     let b = Int16.Parse(hex, Globalization.NumberStyles.AllowHexSpecifier)
     let c = Convert.ToString(b, 2)
-    c
+    let d = c.PadLeft(4, '0')
+    d
 
 
 let solveA (input:string) =
-    hexToBinary input[0]
+    let binaryList = Seq.map(fun i -> hexToBinary i) input
+    let binary = Seq.concat binaryList
+    let binaryText = new String(Seq.toArray binary)
+    printfn "%A" binaryText
+    binaryText
+    // 110100101111111000101000
+
+solveA ex1
 
 
-solveA ex1    
+// reference
+let fromHex (s:string) = 
+  s
+  |> Seq.windowed 2
+  |> Seq.mapi (fun i j -> (i,j))
+  |> Seq.filter (fun (i,j) -> i % 2=0)
+  |> Seq.map (fun (_,j) -> Byte.Parse(new System.String(j),System.Globalization.NumberStyles.AllowHexSpecifier))
+  |> Array.ofSeq
