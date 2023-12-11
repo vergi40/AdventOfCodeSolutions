3e1780ebab00adeef1e6f73581fa872a571c003d
Teemu Laine <teemu.laine@laide.fi>
2023-12-11 11:37:27 +0200

    Day1 F#

---
+++ 2023_Aoc/FSharp/Utils.fs
@@ -0,0 +1,11 @@
+﻿module Utils
+
+open System.IO
+
+// dayName as "01" or "22"
+let GetInput dayName = 
+    let folder = @"C:\Git\AdventOfCodeSolutions\2023_Aoc\Input"
+    let filePath = Path.Combine(folder, dayName + ".txt")
+    File.ReadAllLines(filePath)
+
+    
