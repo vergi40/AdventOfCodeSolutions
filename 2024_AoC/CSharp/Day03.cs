900fc978587f1236b472d1ab64ac578c38de95db
Teemu Laine <teemu.laine@laide.fi>
2025-09-21 23:03:53 +0300

    Day3 B

--- 2024_AoC/CSharp/Day03.cs
+++ 2024_AoC/CSharp/Day03.cs
@@ -1,6 +1,4 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace CSharp
@@ -22,28 +20,10 @@ namespace CSharp
 
             foreach (var mulPrefix in mulPrefixes)
             {
-                // Should have 1-3 integers, ",", 1-3 integers, ")"
-                if(!TryReadNumber(mulPrefix, 0, out var first, out var nextIndex)){
-                    continue;
-                }
-
-                if (nextIndex >= mulPrefix.Length || mulPrefix[nextIndex] != ',')
+                if(TryParseNext(mulPrefix, out var mulResult))
                 {
-                    continue;
+                    result += mulResult;
                 }
-                nextIndex++;
-
-                if (!TryReadNumber(mulPrefix, nextIndex, out var second, out nextIndex))
-                {
-                    continue;
-                }
-
-                if (nextIndex >= mulPrefix.Length || mulPrefix[nextIndex] != ')')
-                {
-                    continue;
-                }
-
-                result += first * second;
             }
 
             Assert.That(result, Is.EqualTo(187194524));
@@ -74,5 +54,130 @@ namespace CSharp
             }
             return false;
         }
+
+        [Test]
+        public void Test2()
+        {
+            var fullInput = string.Concat(Input);
+            var result = 0;
+
+            // mul(x,y)
+            var currentIndex = 0;
+            var enabled = true;
+            while (currentIndex < fullInput.Length)
+            {
+                if (!TryGetNextOperation(fullInput, currentIndex, out var nextOperation, out var nextIndex))
+                {
+                    currentIndex++;
+                    continue;
+                }
+
+                if (nextOperation == Operation.Do)
+                {
+                    enabled = true;
+                }
+                else if (nextOperation == Operation.Dont)
+                {
+                    enabled = false;
+                }
+                else if (nextOperation == Operation.Multiply && enabled)
+                {
+                    // Process mul
+                    if (TryParseNext(fullInput.Substring(nextIndex, 13), out var mulResult))
+                    {
+                        result += mulResult;
+                    }
+
+                }
+
+                currentIndex = nextIndex;
+            }
+
+            Assert.That(result, Is.EqualTo(127092535));
+        }
+
+        private bool TryGetNextOperation(string input, int startIndex, out Operation operation, out int nextIndex)
+        {
+            operation = Operation.Do;
+            nextIndex = startIndex;
+            var validChars = new StringBuilder();
+
+            // do()
+            // don't()
+            // mul(
+            var dict = new Dictionary<string, Operation>
+            {
+                { "do()", Operation.Do },
+                { "don't()", Operation.Dont },
+                { "mul(", Operation.Multiply }
+            };
+
+            foreach (var kvp in dict)
+            {
+                var target = kvp.Key;
+                if(startIndex + target.Length > input.Length)
+                {
+                    // End of input
+                    continue;
+                }
+
+                var isEqual = true;
+                for (int i = 0; i < target.Length; i++)
+                {
+                    var currentIndex = startIndex + i;
+                    if (!input[currentIndex].Equals(target[i]))
+                    {
+                        isEqual = false;
+                        break;
+                    }
+                }
+                // Each char matches target
+                if (isEqual) 
+                {
+                    operation = kvp.Value;
+                    nextIndex += kvp.Key.Length;
+                    return true; 
+                }
+            }
+
+            return false;
+
+        }
+
+        private bool TryParseNext(string input, out int result)
+        {
+            result = 0;
+
+            // Should have 1-3 integers, ",", 1-3 integers, ")"
+            if (!TryReadNumber(input, 0, out var first, out var nextIndex))
+            {
+                return false;
+            }
+
+            if (nextIndex >= input.Length || input[nextIndex] != ',')
+            {
+                return false;
+            }
+            nextIndex++;
+
+            if (!TryReadNumber(input, nextIndex, out var second, out nextIndex))
+            {
+                return false;
+            }
+
+            if (nextIndex >= input.Length || input[nextIndex] != ')')
+            {
+                return false;
+            }
+
+            result = first * second;
+            return true;
+        }
+
+        enum Operation
+        {
+            Do, Dont, Multiply
+        }
+
     }
 }
