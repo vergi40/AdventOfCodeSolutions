589ae476cbb3efb22436f95c269dd301757daac9
Teemu Laine <teemu.laine@laide.fi>
2025-11-30 13:41:25 +0200

    2024 day 1 as F#

---
+++ 2024_AoC/FSharp/Day01.fs
@@ -0,0 +1,63 @@
+﻿module Day01
+
+open NUnit.Framework
+
+[<Test>]
+let A () =
+
+    let parseToTwoNumbers (line: string) = 
+        let split = line.Split("   ")
+        (int split[0], int split[1])
+
+    // Map2: two lists -> function -> one list
+    let sumListItemDiffs list1 list2 =
+        List.map2 (fun x y -> abs(x - y)) list1 list2 |> List.sum
+
+    let input = Utils.GetInputAsList "01"
+
+    // List[left, right]
+    let numbers = input |> List.map parseToTwoNumbers
+
+    // List[left], List[right]
+    let numbersLeft = numbers |> List.map fst
+    let numbersRight = numbers |> List.map snd
+
+    let sorted1 = List.sort numbersLeft
+    let sorted2 = List.sort numbersRight
+
+    let result = sumListItemDiffs sorted1 sorted2
+    Assert.That(result, Is.EqualTo(2166959))
+
+
+[<Test>]
+let B () =
+
+    let parseToTwoNumbers (line: string) = 
+        let split = line.Split("   ")
+        (int split[0], int split[1])
+
+    let timesExist number list =
+        List.filter(fun x -> x = number) list |> List.length
+
+    // The puzzle requires multiplying timeExist with the number itself
+    let similarityScore number list =
+        (timesExist number list) * number
+
+    // Unoptimized and unnecessary. Should use mutable dict which is checked after each addition
+    let countsToDict' leftList rightList =
+        leftList
+        |> List.map (fun number -> (number, timesExist number rightList))
+        |> Map.ofList
+
+    let input = Utils.GetInputAsList "01"
+
+    // List[left, right]
+    let numbers = input |> List.map parseToTwoNumbers
+
+    // List[left], List[right]
+    let numbersLeft = numbers |> List.map fst
+    let numbersRight = numbers |> List.map snd
+
+    // Unoptimized
+    let result = List.map (fun x -> similarityScore x numbersRight) numbersLeft |> List.sum
+    Assert.That(result, Is.EqualTo(23741109))
---
+++ 2024_AoC/FSharp/Utils.fs
@@ -0,0 +1,17 @@
+﻿module Utils
+
+open System.IO
+
+// dayName as "01" or "22"
+let GetInput dayName = 
+    let folder = @"C:\Git\AdventOfCodeSolutions\2024_Aoc\Input"
+    let filePath = Path.Combine(folder, dayName + ".txt")
+    File.ReadAllLines(filePath)
+
+// dayName as "01" or "22"
+let GetInputAsList dayName = 
+    let folder = @"C:\Git\AdventOfCodeSolutions\2024_Aoc\Input"
+    let filePath = Path.Combine(folder, dayName + ".txt")
+    File.ReadAllLines(filePath) |> Array.toList
+
+    
